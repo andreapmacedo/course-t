2a9240cb2283434c5b053457fefd073f4331564f
Andre <aa.andremacedo@gmail.com>
2022-04-06 18:40:32 -0300

    adiciona o dia 10.2

---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/content/c03-lista-de-componentes-e-chaves/c03-ex01/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
+    // const items = shoppingList.map((item) => {
+    //   console.log("item: ", item);
+    //   return (<li>{ item }</li>);
+    // });
+    const items = shoppingList.map((item, index) => (<li key={ index }>{ item }</li>));
+
+    return (
+      <div>
+        <h2>Lista de compras</h2>
+        <ul>
+          { items }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/content/c04-proptypes/c04-ex01/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Greeting from './Greeting';
+
+function App() {
+  return (
+    <Greeting name='andre' lastName='macedo'/>
+  );
+}
+
+export default App;
