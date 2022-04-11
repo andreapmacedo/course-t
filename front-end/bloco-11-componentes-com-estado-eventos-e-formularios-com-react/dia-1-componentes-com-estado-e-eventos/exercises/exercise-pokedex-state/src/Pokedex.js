0854d34f72ebd72068b36812796b4344770b4272
Andre <aa.andremacedo@gmail.com>
2022-04-11 14:19:44 -0300

    adiciona parcial da solucão 1 do pokedex

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercises/exercise-pokedex-state/src/Pokedex.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exercises/exercise-pokedex-state/src/Pokedex.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
-    render() {
-        return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokeIndex: 0,
+      selectedPokemons : this.props.pokemons.filter((pokemon) => pokemon.type === 'Fire')
     }
+  }
+
+  handleState = (...params) => {
+    // console.log(params[0]);
+    this.setState({
+      pokeIndex: params[0]
+    });    
+  }
+
+  handleNext = () => {
+    if(this.state.pokeIndex < this.state.selectedPokemons.length -1)
+      this.handleState(this.state.pokeIndex + 1)
+    else
+      this.handleState(0)
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className="pokedex">
+            {/* {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)} */}
+            <Pokemon key={this.state.pokeIndex} pokemon={this.state.selectedPokemons.at(this.state.pokeIndex)} />)
+        </div>
+        <section>
+          <button onClick={this.handleNext}>Next</button>
+        </section>
+      </Fragment>
+    );
+  }
 }
 
 export default Pokedex;
